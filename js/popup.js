d4c330ea53b73ed88faa6e4a7f47109f6c595d52
Matthew Cross <matthew@pmg.co>
2014-03-05 00:39:33 -0600

    Implemented a popup menu

---
+++ js/popup.js
@@ -0,0 +1,83 @@
+
+(function($) {
+
+  var PopupController = function() {
+    this.deviceList = [];
+  }
+  
+  PopupController.prototype.start = function() {
+    this.registerEvents();
+  }
+  
+  PopupController.prototype.registerEvents = function() {
+    var that = this;
+    chrome.runtime.sendMessage({action: 'getDevices'}, function(res) {
+      if (res.errors && res.errors.length > 0) {
+        console.log('Failed to get devices from Fitbit API service.');
+        return;
+      }
+      that.deviceList = JSON.parse(res.items);
+      that.requestAlarms();
+    });
+  }
+  
+  PopupController.prototype.requestAlarms = function() {
+    var that = this;
+    for (var i in this.deviceList) {
+      chrome.runtime.sendMessage({action: 'getAlarms', args: { deviceId: this.deviceList[i].id }}, function(res) {
+        if (res.errors && res.errors.length > 0) {
+          console.log('Failed to get alarms from Fitbit API service.');
+          return;
+        }
+        that.deviceList[i].alarms = JSON.parse(res.items).trackerAlarms || [];
+        that.buildDeviceList(that.deviceList);
+      });
+    }
+  }
+  
+  PopupController.prototype.buildDeviceList = function(devices) {
+    var list = $('.device-list');
+    for (var i in devices) {
+      list.append(
+        $('<div>')
+          .addClass('device')
+          .attr('data-id', devices[i].id)
+          .append($('<strong>').text(devices[i].deviceVersion))
+          .append(this.buildAlarmList(devices[i].alarms))
+      );
+    }
+  }
+  
+  PopupController.prototype.buildAlarmList = function(alarms) {
+    console.log(alarms);
+    var list = $('<ul>').addClass('alarm-list');
+    for (var i in alarms) {
+        list.append(
+          $('<li>')
+            .addClass('alarm')
+            .attr('data-id', alarms[i].alarmId)
+            .text(alarms[i].time)
+            .append(this.buildRemoveLink())
+        );
+    }
+    return list;
+  }
+  
+  PopupController.prototype.buildRemoveLink = function() {
+    return $('<a>')
+      .attr('href', '#')
+      .addClass('remove')
+      .text('Remove')
+      .click(this.removeAlarm);
+  }
+  
+  PopupController.prototype.removeAlarm = function() {
+    // xxx send deleteAlarm message
+  }
+  
+  $(document).ready(function() {
+    var controller = new PopupController();
+    controller.start();
+  });
+  
+})(window.jQuery);
